3d45cc3360253233c231fa2993bc2bc1bd503ede
hugues.bretin <hugues.bretin@bonitasoft.com>
2020-08-27 15:16:50 +0200

    ci(test): improve tests

--- src/main/java/org/bonitasoft/actorfilter/initiator/manager/ProcessInitiatorManagerUserFilter.java
+++ src/main/java/org/bonitasoft/actorfilter/initiator/manager/ProcessInitiatorManagerUserFilter.java
@@ -1,13 +1,31 @@
+/**
+ * Copyright (C) 2014 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301, USA.
+ **/
 package org.bonitasoft.actorfilter.initiator.manager;
 
 import org.bonitasoft.engine.connector.ConnectorValidationException;
 import org.bonitasoft.engine.exception.BonitaException;
 import org.bonitasoft.engine.filter.AbstractUserFilter;
 import org.bonitasoft.engine.filter.UserFilterException;
+import org.bonitasoft.engine.identity.User;
+import org.bonitasoft.engine.identity.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 /**
  * @author Baptiste Mesta
@@ -17,16 +35,29 @@ public class ProcessInitiatorManagerUserFilter extends AbstractUserFilter {
 
     static final String AUTO_ASSIGN = "autoAssign";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProcessInitiatorManagerUserFilter.class);
+
     @Override
     public void validateInputParameters() throws ConnectorValidationException {
     }
 
     @Override
     public List<Long> filter(final String actorName) throws UserFilterException {
+        final long processInstanceId = getExecutionContext().getProcessInstanceId();
         try {
-            final long processInstanceId = getExecutionContext().getProcessInstanceId();
             long processInitiator = getAPIAccessor().getProcessAPI().getProcessInstance(processInstanceId).getStartedBy();
-            return asList(getAPIAccessor().getIdentityAPI().getUser(processInitiator).getManagerUserId());
+            User user = getAPIAccessor().getIdentityAPI().getUser(processInitiator);
+            long managerUserId = user.getManagerUserId();
+            // A '0' value for the user id means that it is not assign.
+            if (managerUserId == 0) {
+                // If a user has no manager, just return an empty candidate list.
+                LOGGER.warn("No manager found for user: {}", user);
+                return emptyList();
+            }
+            return asList(managerUserId);
+        } catch (final UserNotFoundException e) {
+            LOGGER.warn("No process initiator found for process instance {}. Process may have been started by the system", processInstanceId);
+            throw new UserFilterException(e);
         } catch (final BonitaException e) {
             throw new UserFilterException(e);
         }
@@ -34,8 +65,8 @@ public class ProcessInitiatorManagerUserFilter extends AbstractUserFilter {
 
     @Override
     public boolean shouldAutoAssignTaskIfSingleResult() {
-        final Boolean autoAssignO = (Boolean) getInputParameter(AUTO_ASSIGN);
-        return autoAssignO == null || autoAssignO;
+        final Boolean autoAssign = (Boolean) getInputParameter(AUTO_ASSIGN);
+        return autoAssign == null || autoAssign;
     }
 
 }
--- src/test/java/org/bonitasoft/actorfilter/initiator/manager/ProcessInitiatorManagerUserFilterTest.java
+++ src/test/java/org/bonitasoft/actorfilter/initiator/manager/ProcessInitiatorManagerUserFilterTest.java
@@ -5,7 +5,9 @@ import org.bonitasoft.engine.api.IdentityAPI;
 import org.bonitasoft.engine.api.ProcessAPI;
 import org.bonitasoft.engine.bpm.process.ProcessInstance;
 import org.bonitasoft.engine.connector.EngineExecutionContext;
+import org.bonitasoft.engine.filter.UserFilterException;
 import org.bonitasoft.engine.identity.User;
+import org.bonitasoft.engine.identity.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +23,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.bonitasoft.actorfilter.initiator.manager.ProcessInitiatorManagerUserFilter.AUTO_ASSIGN;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -28,10 +31,10 @@ import static org.mockito.Mockito.when;
 @RunWith(JUnitPlatform.class)
 class ProcessInitiatorManagerUserFilterTest {
 
+    public static final long NO_ID = 0;
     private static final long PROCESS_INSTANCE_ID = 1L;
     private static final Long INITIATOR_ID = 2L;
     private static final Long MANAGER_ID = 3L;
-
     @InjectMocks
     private ProcessInitiatorManagerUserFilter filter;
 
@@ -44,12 +47,17 @@ class ProcessInitiatorManagerUserFilterTest {
 
     @Mock(lenient = true)
     private EngineExecutionContext executionContext;
+    @Mock(lenient = true)
+    private ProcessInstance processInstance;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
         when(apiAccessor.getProcessAPI()).thenReturn(processApi);
         when(apiAccessor.getIdentityAPI()).thenReturn(indentityApi);
         when(executionContext.getProcessInstanceId()).thenReturn(PROCESS_INSTANCE_ID);
+
+        when(processApi.getProcessInstance(PROCESS_INSTANCE_ID)).thenReturn(processInstance);
+        when(processInstance.getStartedBy()).thenReturn(INITIATOR_ID);
     }
 
     @Test
@@ -96,10 +104,6 @@ class ProcessInitiatorManagerUserFilterTest {
     @Test
     void should_return_a_list_of_candidates() throws Exception {
         // Given
-        ProcessInstance processInstance = mock(ProcessInstance.class);
-        when(processApi.getProcessInstance(PROCESS_INSTANCE_ID)).thenReturn(processInstance);
-        when(processInstance.getStartedBy()).thenReturn(INITIATOR_ID);
-
         User initiator = mock(User.class);
         when(indentityApi.getUser(INITIATOR_ID)).thenReturn(initiator);
         when(initiator.getManagerUserId()).thenReturn(MANAGER_ID);
@@ -111,6 +115,33 @@ class ProcessInitiatorManagerUserFilterTest {
         assertThat(candidates).as("Only the manager from the initiating user can be candidate.")
                 .containsExactly(MANAGER_ID);
 
+    }
+
+    @Test
+    void should_return_an_empty_list_if_no_manager_candidates() throws Exception {
+        // Given
+        User initiator = mock(User.class);
+        when(indentityApi.getUser(INITIATOR_ID)).thenReturn(initiator);
+        when(initiator.getManagerUserId()).thenReturn(NO_ID);
+
+        // When
+        List<Long> candidates = filter.filter("MyActor");
+
+        // Then
+        assertThat(candidates).as("Only the manager from the initiating user can be candidate.")
+                .isEmpty();
+
+    }
+
+    @Test
+    void should_throw_ex_if_process_started_by_system() throws Exception {
+        // Given
+        when(indentityApi.getUser(INITIATOR_ID)).thenThrow(new UserNotFoundException("No user found for id:" + INITIATOR_ID));
+
+        // When
+        assertThrows(UserFilterException.class, () ->
+                filter.filter("MyActor")
+        );
     }
 
 }
