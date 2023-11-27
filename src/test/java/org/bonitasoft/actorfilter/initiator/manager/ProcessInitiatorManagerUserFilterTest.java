e974d5ca9bfac12797684a2dbccacf2d3d138094
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-11-27 15:38:59 +0100

    chore(build): use java 17
    Closes BPM-19

--- src/test/java/org/bonitasoft/actorfilter/initiator/manager/ProcessInitiatorManagerUserFilterTest.java
+++ src/test/java/org/bonitasoft/actorfilter/initiator/manager/ProcessInitiatorManagerUserFilterTest.java
@@ -1,5 +1,15 @@
 package org.bonitasoft.actorfilter.initiator.manager;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.bonitasoft.actorfilter.initiator.manager.ProcessInitiatorManagerUserFilter.AUTO_ASSIGN;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.bonitasoft.engine.api.APIAccessor;
 import org.bonitasoft.engine.api.IdentityAPI;
 import org.bonitasoft.engine.api.ProcessAPI;
@@ -11,24 +21,11 @@ import org.bonitasoft.engine.identity.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.bonitasoft.actorfilter.initiator.manager.ProcessInitiatorManagerUserFilter.AUTO_ASSIGN;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 class ProcessInitiatorManagerUserFilterTest {
 
     public static final long NO_ID = 0;
